428d9fd00cb8e577490781eb1c53a3f82bf4acfd
Xiao-Yong Jin <xjin@anl.gov>
2018-10-15 21:43:09 -0500

    collect run scripts and result json files to one repository

---
+++ theta/scala/env_local.sh
@@ -0,0 +1,18 @@
+[[ -d $SPARK_CONF_DIR ]] || mkdir -p "$SPARK_CONF_DIR"
+# JVM support for Xeon TSX (coarse-grain locking) -XX:+UseRTMLocking
+[[ -s $SPARK_CONF_DIR/spark-defaults.conf ]] ||
+	cat > "$SPARK_CONF_DIR/spark-defaults.conf" <<'EOF'
+spark.task.cpus            4
+spark.scheduler.maxRegisteredResourcesWaitingTime            4000s
+spark.scheduler.minRegisteredResourcesRatio            1
+spark.scheduler.listenerbus.eventqueue.capacity            100000
+spark.worker.timeout            24000
+spark.executor.heartbeatInterval            4000s
+spark.files.fetchTimeout            12000s
+spark.network.timeout            24000s
+spark.locality.wait            6000s
+spark.driver.memory              16g
+spark.executor.memory            128g
+spark.driver.extraJavaOptions        -XX:+UseParallelGC -XX:ParallelGCThreads=8
+spark.executor.extraJavaOptions        -XX:+UseParallelGC -XX:ParallelGCThreads=8
+EOF
